e2976d1930a1785c5a044ba00f086cecc9b0b363
pyh <P_18838517198@126.com>
2023-03-20 08:44:07 +0800

    3.20 8:44

--- spring-debug-demo/src/test/java/JavaTest.java
+++ spring-debug-demo/src/test/java/JavaTest.java
@@ -21,4 +21,27 @@ public class JavaTest {
 			System.out.println("我在行走2");
 		});
 	}
+
+	@Test
+	public void test2(){
+		//获取系统类加载器
+		ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+		System.out.println(systemClassLoader);//sun.misc.Launcher$AppClassLoader@4e0e2f2a
+
+		//获取其上层，扩展类加载器
+		ClassLoader extClassLoader = systemClassLoader.getParent();
+		System.out.println(extClassLoader);//sun.misc.Launcher$ExtClassLoader@1055248e
+
+		//获取其上层: 获取不到引导类加载器
+		ClassLoader bootstrapClassLoader = extClassLoader.getParent();
+		System.out.println(bootstrapClassLoader);//null
+
+		//对于用户自定义类来说:默认使用系统类加载器进行加载
+		ClassLoader classLoader = JavaTest.class.getClassLoader();
+		System.out.println(classLoader);//sun.misc.Launcher$AppClassLoader@4e0e2f2a
+
+		//String类使用引导类加载器进行加载的。---->Java的核心类库都是使用引导类加载器进行加载的
+		ClassLoader classLoader1 = String.class.getClassLoader();
+		System.out.println(classLoader1);//null
+	}
 }
