50a54b2c8003fe7361b6225dacbe84043024733c
pyh <P_18838517198@126.com>
2023-04-19 21:55:25 +0800

    4.19 21:55

--- spring-web-debug-xml/src/main/java/controller/ZhouyuBeanNameController.java
+++ spring-web-debug-xml/src/main/java/controller/ZhouyuBeanNameController.java
@@ -1,4 +1,19 @@
 package controller;
 
-public class ZhouyuBeanNameController{
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Component("/test")
+public class ZhouyuBeanNameController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("test zhouyu");
+		return null;
+	}
 }
---
+++ spring-web-debug-xml/src/main/java/controller/ZhouyuControllerAdvice.java
@@ -0,0 +1,38 @@
+package controller;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+@Component
+@ControllerAdvice
+// RequestBodyAdvice相当于一个切面，处理请求之前调那些方法做一些额外的处理。
+// ResponseBodyAdvice 跟AOP很像，但是springmvc自己实现的。写响应体之前我可以额外做一些事情。
+// 这些都是一些扩展的机制。初始化的时候看你有没有实现这些接口，如果实现了他就会记录下来。
+public class ZhouyuControllerAdvice implements RequestBodyAdvice {
+	@Override
+	public boolean supports(MethodParameter methodParameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+		return false;
+	}
+
+	@Override
+	public HttpInputMessage beforeBodyRead(HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) throws IOException {
+		return null;
+	}
+
+	@Override
+	public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+		return null;
+	}
+
+	@Override
+	public Object handleEmptyBody(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+		return null;
+	}
+}
